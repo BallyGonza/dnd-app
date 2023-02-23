f6d0f11293838c5e0b5076a908653a2d580ddd77
Gonzalo Bally <gonzalo.bally@urbetrack.com>
2023-02-23 13:20:30 -0300

    Fixes

--- dnd_app/lib/main.dart
+++ dnd_app/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import '../screens/character_select.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
   runApp(
     const MyApp(),
   );
