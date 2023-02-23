df4dc4486ba0276b0fdbd3562e6c73864c172750
Gonzalo Bally <gonzalo.bally@urbetrack.com>
2023-02-23 16:38:44 -0300

    Initial steps hive

--- dnd_app/lib/main.dart
+++ dnd_app/lib/main.dart
@@ -1,14 +1,16 @@
+import 'package:dnd_app/screens/character_select.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
-import '../screens/character_select.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
   );
+  await Hive.initFlutter();
   runApp(
     const MyApp(),
   );
