b19c7e963e70650d361fb8d094ecf667e1094e6c
Gonzalo Bally <gonzalo.bally@urbetrack.com>
2023-02-24 17:15:14 -0300

    Update note_list.dart

--- dnd_app/lib/components/note/note_list.dart
+++ dnd_app/lib/components/note/note_list.dart
@@ -28,8 +28,10 @@ class NoteList extends StatelessWidget {
                         return GestureDetector(
                           onTap: () {
                             showModalBottomSheet(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(16),
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.vertical(
+                                    top: Radius.circular(10),
+                                  ),
                                 ),
                                 context: context,
                                 builder: (context) {
@@ -63,7 +65,8 @@ class NoteList extends StatelessWidget {
                                             note.content = value;
                                           },
                                           controller: TextEditingController(
-                                              text: note.content),
+                                            text: note.content,
+                                          ),
                                           maxLines: 11,
                                           decoration: const InputDecoration(
                                             border: OutlineInputBorder(),
@@ -74,45 +77,57 @@ class NoteList extends StatelessWidget {
                                             ),
                                           ),
                                         ),
-                                        const SizedBox(
-                                          height: 10,
-                                        ),
+                                        Spacer(),
                                         Row(
                                           mainAxisAlignment:
                                               MainAxisAlignment.spaceBetween,
                                           children: [
-                                            ElevatedButton(
-                                              onPressed: () {
-                                                // update note
-                                                box.putAt(
-                                                  index,
-                                                  Note(
-                                                    title:
-                                                        TextEditingController(
-                                                                text:
-                                                                    note.title)
-                                                            .text,
-                                                    content:
-                                                        TextEditingController(
-                                                                text: note
-                                                                    .content)
-                                                            .text,
-                                                    date: DateTime.now()
-                                                        .toString(),
-                                                    color: note.color,
-                                                  ),
-                                                );
-                                                Navigator.pop(context);
-                                              },
-                                              child: const Text('Update'),
+                                            SizedBox(
+                                              width: MediaQuery.of(context)
+                                                      .size
+                                                      .width *
+                                                  0.4,
+                                              child: ElevatedButton(
+                                                style: ElevatedButton.styleFrom(
+                                                  backgroundColor: Colors.black,
+                                                ),
+                                                onPressed: () {
+                                                  box.putAt(
+                                                    index,
+                                                    Note(
+                                                      title:
+                                                          TextEditingController(
+                                                        text: note.title,
+                                                      ).text,
+                                                      content:
+                                                          TextEditingController(
+                                                        text: note.content,
+                                                      ).text,
+                                                      date: DateTime.now()
+                                                          .toString(),
+                                                      color: note.color,
+                                                    ),
+                                                  );
+                                                  Navigator.pop(context);
+                                                },
+                                                child: const Text('Update'),
+                                              ),
                                             ),
-                                            ElevatedButton(
-                                              onPressed: () {
-                                                // delete note
-                                                box.deleteAt(index);
-                                                Navigator.pop(context);
-                                              },
-                                              child: const Text('Delete'),
+                                            SizedBox(
+                                              width: MediaQuery.of(context)
+                                                      .size
+                                                      .width *
+                                                  0.4,
+                                              child: ElevatedButton(
+                                                style: ElevatedButton.styleFrom(
+                                                  backgroundColor: Colors.black,
+                                                ),
+                                                onPressed: () {
+                                                  box.deleteAt(index);
+                                                  Navigator.pop(context);
+                                                },
+                                                child: const Text('Delete'),
+                                              ),
                                             ),
                                           ],
                                         ),
@@ -134,90 +149,96 @@ class NoteList extends StatelessWidget {
           },
         ),
         // button add note
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                // open modal bottom sheet
-                showModalBottomSheet(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    context: context,
-                    builder: (context) {
-                      return Container(
-                        padding: const EdgeInsets.all(16),
-                        child: Column(
-                          children: [
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            TextField(
-                              controller: titleController,
-                              decoration: const InputDecoration(
-                                hintText: 'Title',
-                                border: OutlineInputBorder(),
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            TextField(
-                              controller: contentController,
-                              maxLines: 11,
-                              decoration: const InputDecoration(
-                                hintText: 'Content',
-                                border: OutlineInputBorder(),
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                // add note to box
-                                Hive.box<Note>('notes_box').add(
-                                  Note(
-                                    title: titleController.text,
-                                    color: Colors.lightGreen.value,
-                                    date: DateTime.now().toString(),
-                                    content: contentController.text,
-                                  ),
-                                );
-
-                                // close modal bottom sheet
-                                Navigator.pop(context);
-                              },
-                              child: const Text('Add Note'),
-                            ),
-                          ],
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.black,
+              shape: const CircleBorder(),
+            ),
+            onLongPress: () {
+              Hive.box<Note>('notes_box').deleteAll(
+                Hive.box<Note>('notes_box').keys,
+              );
+            },
+            onPressed: () {
+              showModalBottomSheet(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                context: context,
+                builder: (context) {
+                  return Container(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            'Add Loot',
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
                         ),
-                      );
-                    });
-              },
-              child: const Text('Add Note'),
-            ),
-            IconButton(
-              onPressed: () {
-                // delete all notes
-                Hive.box<Note>('notes_box').deleteAll(
-                  Hive.box<Note>('notes_box').keys,
-                );
-              },
-              icon: const Icon(Icons.delete),
-            ),
-          ],
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          controller: titleController,
+                          decoration: const InputDecoration(
+                            hintText: 'Title',
+                            border: OutlineInputBorder(),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          controller: contentController,
+                          maxLines: 11,
+                          decoration: const InputDecoration(
+                            hintText: 'Content',
+                            border: OutlineInputBorder(),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const Spacer(),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black,
+                            ),
+                            onPressed: () {
+                              Hive.box<Note>('notes_box').add(
+                                Note(
+                                  title: titleController.text,
+                                  color: Colors.lightGreen.value,
+                                  date: DateTime.now().toString(),
+                                  content: contentController.text,
+                                ),
+                              );
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Add Note'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+            child: const Icon(Icons.add),
+          ),
         ),
         // button delete note
       ],
