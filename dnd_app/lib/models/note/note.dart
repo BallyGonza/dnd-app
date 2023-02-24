8ed9bd5fb63c2a4aa5b6316ed178762ed6bec1c3
Gonzalo Bally <gonzalo.bally@urbetrack.com>
2023-02-24 11:28:47 -0300

    Added note model

---
+++ dnd_app/lib/models/note/note.dart
@@ -0,0 +1,22 @@
+import 'package:hive/hive.dart';
+
+part 'note.g.dart';
+
+@HiveType(typeId: 0)
+class Note extends HiveObject {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String content;
+  @HiveField(2)
+  final String date;
+  @HiveField(3)
+  final int color;
+
+  Note({
+    required this.title,
+    required this.content,
+    required this.date,
+    required this.color,
+  });
+}
