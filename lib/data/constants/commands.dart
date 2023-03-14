000f8432b3334ea97a528a7e765a212d69194465
Gonzalo Bally <gonzalo.bally@urbetrack.com>
2023-03-14 18:56:17 -0300

    Create commands.dart

---
+++ lib/data/constants/commands.dart
@@ -0,0 +1,3 @@
+// Comando para generar iconos: flutter pub run flutter_launcher_icons:main
+// Comando para generar splash screen: flutter pub run flutter_native_splash:create
+// Comando para generar release en ios: flutter build --release
