565c5766153e46fb56491a709541a87682d394f4
Gonzalo Bally <gonzalo.bally@urbetrack.com>
2023-03-17 19:50:18 -0300

    Working

--- lib/views/widgets/wallet/wallet_list.dart
+++ lib/views/widgets/wallet/wallet_list.dart
@@ -33,11 +33,12 @@ class _WalletListState extends State<WalletList> {
                     context: context,
                     builder: (_) {
                       return AlertDialog(
-                        title: Text(
+                        title: const Text(
                           'Enter Amount',
                         ),
                         content: TextField(
                           keyboardType: TextInputType.number,
+                          // ignore: prefer_const_constructors
                           decoration: InputDecoration(
                             hintText: 'Enter Amount',
                           ),
@@ -62,7 +63,7 @@ class _WalletListState extends State<WalletList> {
                               );
                               Navigator.of(_).pop();
                             },
-                            child: Text(
+                            child: const Text(
                               'Ok',
                             ),
                           ),
@@ -89,66 +90,250 @@ class _WalletListState extends State<WalletList> {
                   color: const Color(0xFFB87333),
                 ),
               ),
-              // WalletListItem(
-              //   coinType: 'Plata',
-              //   coinAmount: state.wallet.silverPieces,
-              //   onAdd: () => setState(() {
-              //     BlocProvider.of<WalletBloc>(context).add(
-              //       const WalletEvent.add('silver'),
-              //     );
-              //   }),
-              //   onSubtract: () => setState(() {
-              //     BlocProvider.of<WalletBloc>(context).add(
-              //       const WalletEvent.subtract('silver'),
-              //     );
-              //   }),
-              //   color: const Color(0xFFC0C0C0),
-              // ),
-              // WalletListItem(
-              //   coinType: 'Electrum',
-              //   coinAmount: state.wallet.electrumPieces,
-              //   onAdd: () => setState(() {
-              //     BlocProvider.of<WalletBloc>(context).add(
-              //       const WalletEvent.add('electrum'),
-              //     );
-              //   }),
-              //   onSubtract: () => setState(() {
-              //     BlocProvider.of<WalletBloc>(context).add(
-              //       const WalletEvent.subtract('electrum'),
-              //     );
-              //   }),
-              //   color: const Color(0xFFC0D9D9),
-              // ),
-              // WalletListItem(
-              //   coinType: 'Oro',
-              //   coinAmount: state.wallet.goldPieces,
-              //   onAdd: () => setState(() {
-              //     BlocProvider.of<WalletBloc>(context).add(
-              //       const WalletEvent.add('gold'),
-              //     );
-              //   }),
-              //   onSubtract: () => setState(() {
-              //     BlocProvider.of<WalletBloc>(context).add(
-              //       const WalletEvent.subtract('gold'),
-              //     );
-              //   }),
-              //   color: const Color(0xFFD4AF37),
-              // ),
-              // WalletListItem(
-              //   coinType: 'Platino',
-              //   coinAmount: state.wallet.platinumPieces,
-              //   onAdd: () => setState(() {
-              //     BlocProvider.of<WalletBloc>(context).add(
-              //       const WalletEvent.add('platinum'),
-              //     );
-              //   }),
-              //   onSubtract: () => setState(() {
-              //     BlocProvider.of<WalletBloc>(context).add(
-              //       const WalletEvent.subtract('platinum'),
-              //     );
-              //   }),
-              //   color: const Color(0xFFE5E4E2),
-              // ),
+              InkWell(
+                onTap: () => setState(() {
+                  showDialog(
+                    context: context,
+                    builder: (_) {
+                      return AlertDialog(
+                        title: const Text(
+                          'Enter Amount',
+                        ),
+                        content: TextField(
+                          keyboardType: TextInputType.number,
+                          // ignore: prefer_const_constructors
+                          decoration: InputDecoration(
+                            hintText: 'Enter Amount',
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              state.wallet.silverPieces = int.parse(value);
+                            });
+                          },
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              setState(
+                                () {
+                                  BlocProvider.of<WalletBloc>(context).add(
+                                    WalletEvent.set(
+                                      'silver',
+                                      state.wallet.silverPieces,
+                                    ),
+                                  );
+                                },
+                              );
+                              Navigator.of(_).pop();
+                            },
+                            child: const Text(
+                              'Ok',
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }),
+                child: WalletListItem(
+                  coinType: 'Plata',
+                  coinAmount: state.wallet.silverPieces,
+                  onAdd: () => setState(() {
+                    BlocProvider.of<WalletBloc>(context).add(
+                      const WalletEvent.add('silver'),
+                    );
+                  }),
+                  onSubtract: () => setState(() {
+                    BlocProvider.of<WalletBloc>(context).add(
+                      const WalletEvent.subtract('silver'),
+                    );
+                  }),
+                  color: const Color(0xFFC0C0C0),
+                ),
+              ),
+              InkWell(
+                onTap: () => setState(() {
+                  showDialog(
+                    context: context,
+                    builder: (_) {
+                      return AlertDialog(
+                        title: const Text(
+                          'Enter Amount',
+                        ),
+                        content: TextField(
+                          keyboardType: TextInputType.number,
+                          // ignore: prefer_const_constructors
+                          decoration: InputDecoration(
+                            hintText: 'Enter Amount',
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              state.wallet.electrumPieces = int.parse(value);
+                            });
+                          },
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              setState(
+                                () {
+                                  BlocProvider.of<WalletBloc>(context).add(
+                                    WalletEvent.set(
+                                      'electrum',
+                                      state.wallet.electrumPieces,
+                                    ),
+                                  );
+                                },
+                              );
+                              Navigator.of(_).pop();
+                            },
+                            child: const Text(
+                              'Ok',
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }),
+                child: WalletListItem(
+                  coinType: 'Electrum',
+                  coinAmount: state.wallet.electrumPieces,
+                  onAdd: () => setState(() {
+                    BlocProvider.of<WalletBloc>(context).add(
+                      const WalletEvent.add('electrum'),
+                    );
+                  }),
+                  onSubtract: () => setState(() {
+                    BlocProvider.of<WalletBloc>(context).add(
+                      const WalletEvent.subtract('electrum'),
+                    );
+                  }),
+                  color: const Color(0xFFC0D9D9),
+                ),
+              ),
+              InkWell(
+                onTap: () => setState(() {
+                  showDialog(
+                    context: context,
+                    builder: (_) {
+                      return AlertDialog(
+                        title: const Text(
+                          'Enter Amount',
+                        ),
+                        content: TextField(
+                          keyboardType: TextInputType.number,
+                          // ignore: prefer_const_constructors
+                          decoration: InputDecoration(
+                            hintText: 'Enter Amount',
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              state.wallet.goldPieces = int.parse(value);
+                            });
+                          },
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              setState(
+                                () {
+                                  BlocProvider.of<WalletBloc>(context).add(
+                                    WalletEvent.set(
+                                      'gold',
+                                      state.wallet.goldPieces,
+                                    ),
+                                  );
+                                },
+                              );
+                              Navigator.of(_).pop();
+                            },
+                            child: const Text(
+                              'Ok',
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }),
+                child: WalletListItem(
+                  coinType: 'Oro',
+                  coinAmount: state.wallet.goldPieces,
+                  onAdd: () => setState(() {
+                    BlocProvider.of<WalletBloc>(context).add(
+                      const WalletEvent.add('gold'),
+                    );
+                  }),
+                  onSubtract: () => setState(() {
+                    BlocProvider.of<WalletBloc>(context).add(
+                      const WalletEvent.subtract('gold'),
+                    );
+                  }),
+                  color: const Color(0xFFD4AF37),
+                ),
+              ),
+              InkWell(
+                onTap: () => setState(() {
+                  showDialog(
+                    context: context,
+                    builder: (_) {
+                      return AlertDialog(
+                        title: const Text(
+                          'Enter Amount',
+                        ),
+                        content: TextField(
+                          keyboardType: TextInputType.number,
+                          // ignore: prefer_const_constructors
+                          decoration: InputDecoration(
+                            hintText: 'Enter Amount',
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              state.wallet.platinumPieces = int.parse(value);
+                            });
+                          },
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              setState(
+                                () {
+                                  BlocProvider.of<WalletBloc>(context).add(
+                                    WalletEvent.set(
+                                      'platinum',
+                                      state.wallet.platinumPieces,
+                                    ),
+                                  );
+                                },
+                              );
+                              Navigator.of(_).pop();
+                            },
+                            child: const Text(
+                              'Ok',
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }),
+                child: WalletListItem(
+                  coinType: 'Platino',
+                  coinAmount: state.wallet.platinumPieces,
+                  onAdd: () => setState(() {
+                    BlocProvider.of<WalletBloc>(context).add(
+                      const WalletEvent.add('platinum'),
+                    );
+                  }),
+                  onSubtract: () => setState(() {
+                    BlocProvider.of<WalletBloc>(context).add(
+                      const WalletEvent.subtract('platinum'),
+                    );
+                  }),
+                  color: const Color(0xFFE5E4E2),
+                ),
+              ),
             ],
           ),
         );
