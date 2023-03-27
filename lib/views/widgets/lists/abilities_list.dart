08cbb5a2e148368836a4d4127de24b8cfa98aca9
Gonzalo Bally <gonzalo.bally@urbetrack.com>
2023-03-27 18:26:48 -0300

    Working

--- lib/views/widgets/lists/abilities_list.dart
+++ lib/views/widgets/lists/abilities_list.dart
@@ -12,42 +12,46 @@ class AbilitiesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 15),
-      height: abilities.length > 4
-          ? MediaQuery.of(context).size.height * 0.40
-          : MediaQuery.of(context).size.height * 0.15,
-      child: Card(
-        elevation: 5,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 20, left: 20, bottom: 10),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Abilities',
-                  style: Theme.of(context).textTheme.headline3,
+    return Column(
+      children: [
+        Container(
+          height: abilities.length > 4
+              ? MediaQuery.of(context).size.height * 0.40
+              : MediaQuery.of(context).size.height * 0.15,
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 20, bottom: 10),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Abilities',
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                  ),
                 ),
-              ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.all(0),
+                  itemCount: abilities.length,
+                  itemBuilder: (context, index) {
+                    return AbilityCard(
+                      ability: abilities[index],
+                      savingThrow: savingThrows[index],
+                    );
+                  },
+                ),
+              ],
             ),
-            ListView.builder(
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(0),
-              itemCount: abilities.length,
-              itemBuilder: (context, index) {
-                return AbilityCard(
-                  ability: abilities[index],
-                  savingThrow: savingThrows[index],
-                );
-              },
-            ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
--- lib/views/widgets/lists/skills_list.dart
+++ lib/views/widgets/lists/skills_list.dart
@@ -9,45 +9,49 @@ class SkillsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 15),
-      height: skills.length > 4
-          ? MediaQuery.of(context).size.height * 0.50
-          : MediaQuery.of(context).size.height * 0.15,
-      child: Card(
-        elevation: 5,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 20, left: 20, bottom: 10),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Skills',
-                  style: Theme.of(context).textTheme.headline3,
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          height: skills.length > 4
+              ? MediaQuery.of(context).size.height * 0.50
+              : MediaQuery.of(context).size.height * 0.15,
+          child: Card(
+            elevation: 5,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, left: 20, bottom: 10),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Skills',
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                  ),
                 ),
-              ),
+                GridView.builder(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.all(0),
+                  itemCount: skills.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 5,
+                  ),
+                  itemBuilder: (context, index) {
+                    return SkillCard(
+                      skill: skills[index],
+                    );
+                  },
+                ),
+              ],
             ),
-            GridView.builder(
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(0),
-              itemCount: skills.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 5,
-              ),
-              itemBuilder: (context, index) {
-                return SkillCard(
-                  skill: skills[index],
-                );
-              },
-            ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
